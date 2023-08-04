27f3d70e3edd007c4ce8f5f174b8e8ccc0aacb52
Alfredo <alfredcr19@hotmail.com>
2023-08-04 00:16:56 -0600

    Branch y errores de flake8

---
+++ Tarea 1/errores_flake8.py
@@ -0,0 +1,4 @@
+def errores_flake8():
+    print("Hay varios errores") # si nos pasamos de la columna 79 de la linea de codigo hay error de formato
+
+errores_flake8()  # Este archivo contiene 3 errores de flake8 E261 E501 E305
